934a8c363ef4ba7506155729c976c30bff2fe768
sherwin-dc <swd18@ic.ac.uk>
2020-11-09 14:41:19 +0800

    Add get method

--- BinTree.hpp
+++ BinTree.hpp
@@ -15,15 +15,9 @@ namespace Tree {
       item = nullptr;
     }
     ~node() {
-      if (zero != nullptr) {
-        delete zero;
-      }
-      if (one != nullptr) {
-        delete one;
-      }
-      if (item != nullptr) {
-        delete item;
-      }
+      delete zero;
+      delete one;
+      delete item;
     }
   };
 
@@ -31,14 +25,21 @@ namespace Tree {
   class BinMap {
     private:
       node * root;
-      void New_Traverse(node<T> * &nd, size_t key);
+      void safe_traverse(node<T> * &nd, size_t key);
+      void quick_traverse(node<T> * &nd, size_t key);
     public:
       BinMap();
       ~BinMap();
-      /* Stores a given key and a copy of a value in the map, overiding any existing value*/
-      void Insert(const size_t &key, const T data);
-      T& operator[] (const size_t &key);
-      T& Get(const size_t &key);
+      /*  Stores a given key and a dynamically allocated copy of a value in the map, overiding any existing value */
+      void insert(const size_t &key, const T &data);
+      /*  Returns the (reference of) atomic pointer to the value stored by the key. 
+          This pointer is nullptr if no value had been inserted, and can be used to subsequently set or change an inserted value.
+          This is equivalent to an insert without setting a value */
+      std::atomic<T *> & query(const size_t &key);
+      // T& operator[] (const size_t &key);
+      /*  Gets a value that is guarenteed to already be in the map
+          This is faster than operator[] but unsafe if value has not already been inserted */
+      T& get(const size_t &key);
   };
 
   template<typename T>
@@ -52,32 +53,46 @@ namespace Tree {
   }
 
   template<typename T>
-  void BinMap<T>::Insert(const size_t &key, const T data) {
+  void BinMap<T>::insert(const size_t &key, const T &data) {
 
     node<T> * nd = root;
-    BinMap<T>::New_Traverse(nd, key);
-
+    BinMap<T>::safe_traverse(nd, key);
+    T * new_value = new T(data);
     // Will overwrite any existing data assignment
-    nd->item.store(&data, std::memory_order_relaxed);
+    nd->item.store(new_value, std::memory_order_relaxed);
 
   }
 
-
   template<typename T>
-  T& BinMap<T>::operator[] (const size_t &key) {
-    size_t key_copy = key;
-    data_struct<T> d = RecurFetch(key_copy, root);
-    return d.item;
+  std::atomic<T *> & BinMap<T>::query(const size_t &key) {
+    node<T> * nd = root;
+    BinMap<T>::safe_traverse(nd, key);
+
+    // Returns reference of node's pointer to T
+    return nd->item;
+
+  }
+
+  // template<typename T>
+  // T& BinMap<T>::operator[] (const size_t &key) {
+  //   node<T> * nd = root;
+  //   BinMap<T>::safe_traverse(nd, key);
+
+  //   // Return reference
+  // }
+  
+  template<typename T>
+  T& BinMap<T>::get(const size_t &key) {
+    node<T> * nd = root;
+    BinMap<T>::quick_traverse(nd, key);
+
+    // Return T
+    T * value = nd->item.load(std::memory_order_relaxed);
+    return *value;
   }
   
   template<typename T>
-  data_struct<T>& BinMap<T>::Get(const size_t &key) {
-    size_t key_copy = key;
-    data_struct<T> d = RecurFetch(key_copy, root);
-    return d;
-  }
-      
-  void BinMap<T>::New_Traverse(node<T> * &nd, size_t key) {
+  void BinMap<T>::safe_traverse(node<T> * &nd, size_t key) {
 
     node<T> * new_node;
     node<T> * expected_node;
@@ -122,6 +137,21 @@ namespace Tree {
       key = key >> 1;
       
     }
+  }
+
+  template<typename T>
+  void BinMap<T>::quick_traverse(node<T> * &nd, size_t key) {
+
+    while (key) {
+      // Check if LSB of key is 1 or 0
+      if (key & 1) {
+        nd = nd->one.load(std::memory_order_relaxed);
+      } else {
+        nd = nd->zero.load(std::memory_order_relaxed);
+      }
+      key = key >> 1;
+    }
+
   }
 
 
