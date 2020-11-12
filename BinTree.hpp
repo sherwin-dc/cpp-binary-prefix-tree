1dd1f3e0ca80fee85a9b81124284c09f86a62355
sherwin-dc <swd18@ic.ac.uk>
2020-11-12 18:26:39 +0800

    Change class name to ConcurrentBinMap

--- BinTree.hpp
+++ BinTree.hpp
@@ -1,6 +1,6 @@
 #include <atomic>
 
-namespace Tree {
+namespace BinTree {
 
 
   template<typename T>
@@ -22,41 +22,40 @@ namespace Tree {
   };
 
   template<typename T>
-  class BinMap {
+  class ConcurentBinMap {
     private:
       node<T> * root;
       void safe_traverse(node<T> * &nd, size_t key);
       void quick_traverse(node<T> * &nd, size_t key);
     public:
-      BinMap();
-      ~BinMap();
+      ConcurentBinMap();
+      ~ConcurentBinMap();
       /*  Stores a given key and a dynamically allocated copy of a value in the map, overiding any existing value */
       void insert(const size_t &key, const T &data);
       /*  Returns the (reference of) atomic pointer to the value stored by the key. 
           This pointer is nullptr if no value had been inserted, and can be used to subsequently set or change an inserted value.
           This is equivalent to an insert without setting a value */
       std::atomic<T *> & query(const size_t &key);
-      // T& operator[] (const size_t &key);
       /*  Gets a value that is guarenteed to already be in the map
-          This is faster than operator[] but unsafe if value has not already been inserted */
+          This fast but unsafe if value has not already been inserted */
       T& get(const size_t &key);
   };
 
   template<typename T>
-  BinMap<T>::BinMap() {
+  ConcurentBinMap<T>::ConcurentBinMap() {
     root = new node<T>;
   }
 
   template<typename T>
-  BinMap<T>::~BinMap() {
+  ConcurentBinMap<T>::~ConcurentBinMap() {
     delete root;
   }
 
   template<typename T>
-  void BinMap<T>::insert(const size_t &key, const T &data) {
+  void ConcurentBinMap<T>::insert(const size_t &key, const T &data) {
 
     node<T> * nd = root;
-    BinMap<T>::safe_traverse(nd, key);
+    ConcurentBinMap<T>::safe_traverse(nd, key);
     T * new_value = new T(data);
     // Will overwrite any existing data assignment
     nd->item.store(new_value, std::memory_order_relaxed);
@@ -64,27 +63,20 @@ namespace Tree {
   }
 
   template<typename T>
-  std::atomic<T *> & BinMap<T>::query(const size_t &key) {
+  std::atomic<T *> & ConcurentBinMap<T>::query(const size_t &key) {
     node<T> * nd = root;
-    BinMap<T>::safe_traverse(nd, key);
+    ConcurentBinMap<T>::safe_traverse(nd, key);
 
     // Returns reference of node's pointer to T
     return nd->item;
 
   }
 
-  // template<typename T>
-  // T& BinMap<T>::operator[] (const size_t &key) {
-  //   node<T> * nd = root;
-  //   BinMap<T>::safe_traverse(nd, key);
-
-  //   // Return reference
-  // }
   
   template<typename T>
-  T& BinMap<T>::get(const size_t &key) {
+  T& ConcurentBinMap<T>::get(const size_t &key) {
     node<T> * nd = root;
-    BinMap<T>::quick_traverse(nd, key);
+    ConcurentBinMap<T>::quick_traverse(nd, key);
 
     // Return T
     T * value = nd->item.load(std::memory_order_relaxed);
@@ -92,7 +84,7 @@ namespace Tree {
   }
   
   template<typename T>
-  void BinMap<T>::safe_traverse(node<T> * &nd, size_t key) {
+  void ConcurentBinMap<T>::safe_traverse(node<T> * &nd, size_t key) {
 
     node<T> * new_node;
     node<T> * expected_node;
@@ -140,7 +132,7 @@ namespace Tree {
   }
 
   template<typename T>
-  void BinMap<T>::quick_traverse(node<T> * &nd, size_t key) {
+  void ConcurentBinMap<T>::quick_traverse(node<T> * &nd, size_t key) {
 
     while (key) {
       // Check if LSB of key is 1 or 0
